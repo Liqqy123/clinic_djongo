e496bba2c2c84d84c147f132cdfc3b0781693fac
Ilya <ilamajor21099@gmail.com>
2026-05-12 16:19:06 +0300

    aa

--- clinic_djongo/settings.py
+++ clinic_djongo/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'main'
 ]
+
 FAKER_LOCALE = None
 FAKER_PROVIDERS = None
 MIDDLEWARE = [
